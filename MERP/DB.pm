58cb593da05c2a472a61f5293e5c9e69305683bc
root <root@mareks-test>
2021-11-24 16:11:39 +0000

    added connect function as well as auth detail passing

--- MERP/DB.pm
+++ MERP/DB.pm
@@ -5,15 +5,12 @@ use feature 'say';
 
 use Exporter qw(import);
 our @EXPORT      = qw(db_do db_exec);	# always exported
-our @EXPORT_OK   = qw(); 		# optional to export
+our @EXPORT_OK   = qw(connect);		# optional to export
 
 use DBIx::Connector;
-use Data::Dumper;
 
-# TODO: how do we pass this during startup?
-# simple auth hash used to connect to dbs:
-
-my $auth = {
+my $conn;
+my $default_auth = {
 	type => 'SQLite',
 	name => ':memory:',
 	user => '',
@@ -22,23 +19,32 @@ my $auth = {
 	port => 5432
 };
 
-my $conn = DBIx::Connector->new(
-	sprintf('dbi:%s:dbname=%s;host=%s;port=%d', @{ $auth }{ qw/type name host port/ }, ), $$auth{user}, $$auth{pass},
-	{ AutoCommit => 1, RaiseError => 0, sqlite_see_if_its_a_number => 1 } 
-) or die $DBI::errstr;
-# what is the default, anways?
-$conn->mode('fixup');
+sub connect {
+	my $auth = shift || {};
+	# merge auth keys with default values:
+	if ( ref $auth eq 'HASH' ) {
+		for ( keys %{ $default_auth } ) {
+			$$auth{ $_ } //= $$default_auth{ $_ };
+		}
+	}
 
-if ( $$auth{type} eq 'Pg' ) {
-	db_do( "SET TIMEZONE 'UTC';" );
+	$conn = DBIx::Connector->new(
+		sprintf('dbi:%s:dbname=%s;host=%s;port=%d', @{ $auth }{ qw/type name host port/ }, ), $$auth{user}, $$auth{pass},
+		{ AutoCommit => 1, RaiseError => 0, sqlite_see_if_its_a_number => 1 } 
+	) or die $DBI::errstr;
+
+	$conn->mode('fixup');
+
+	if ( $$auth{type} eq 'Pg' ) {
+		db_do( "SET TIME ZONE 'UTC';" );
+	}
 }
 
 # can call this as: $sth = db_exec('SELECT * FROM TABLE WHERE ID = ?;', 1337 );
 # and collect results: $sth->fetchrow_hashref() && $sth->finish;
-
 sub db_exec {
 	my $query = shift;
-	my $sth = $conn->run( sub { $_->prepare_cached($query); } );
+	my $sth = $conn->run( sub { $_->prepare($query); } );
 	if ( not defined wantarray() ) {
 		$sth->finish;
 		return undef;
